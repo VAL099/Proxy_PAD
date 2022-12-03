79125b5e96f79d62682d3b7c7593e90d1eb9a774
VAL099 <valeriy099@yahoo.com>
2022-12-03 21:29:41 +0200

    Initialising...

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
-S1 = "https://pad-server1.herokuapp.com/"
-S2 = "https://pad-server2.herokuapp.com/"
+S1 = "https://pad-server1.herokuapp.com"
+S2 = "https://pad-server2.herokuapp.com"
 
 AUTH_TOKEN = '09f90c6d05f156dfe301cb49e08834fbab5ea662b0b550dd5f4a2e84418372c5' # LL2_PAD
